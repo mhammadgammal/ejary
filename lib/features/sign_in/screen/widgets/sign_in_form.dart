148978338603c5f5f2da268b740df4cf9d52c4af
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-26 02:07:24 +0200

    Refactor: Update SignIn form widget
    - Wrap the form in a `Form` widget for better form management.
    - Remove the `SizedBox` wrapper around the form.
    - Add horizontal padding to the form's content.
    - Update the layout to use `mainAxisAlignment: MainAxisAlignment.center`.
    - Add an arrow icon to the "Sign In" button.
    - Adjust vertical spacing.

--- lib/features/sign_in/screen/widgets/sign_in_form.dart
+++ lib/features/sign_in/screen/widgets/sign_in_form.dart
@@ -1,4 +1,6 @@
+import 'package:ejary/core/assets/app_icons.dart';
 import 'package:ejary/core/assets/app_images.dart';
+import 'package:ejary/core/theme/app_color.dart';
 import 'package:ejary/core/utils/extensions/string_extensions.dart';
 import 'package:ejary/core/widgets/buttons/custom_filled_button.dart';
 import 'package:ejary/core/widgets/text_form_field/email_text_form_field.dart';
@@ -18,93 +20,91 @@ class SignInForm extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         var cubit = SignInCubit.get(context);
-        return SizedBox(
-          width: 550.w,
+        return Form(
+          key: cubit.formKey,
           child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 40.0,
-              vertical: 20.0,
-            ),
-            child: Form(
-              key: cubit.formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  // TextButton.icon(
-                  //   onPressed: () {
-                  //     final nextLocale =
-                  //         sl<CacheHelper>().getString(
-                  //                   key: CacheKeys.languageCode,
-                  //                 ) ==
-                  //                 'en'
-                  //             ? 'ar'
-                  //             : 'en';
-                  //     log("SignInForm: change language to $nextLocale");
-                  //     AppCubit.get(context).changeLanguage(Locale(nextLocale));
-                  //   },
-                  //   label: Text(
-                  //     AppCubit.get(context).locale.languageCode == 'en'
-                  //         ? "العربيه"
-                  //         : "English",
-                  //     style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  //       fontSize: 22.sp,
-                  //       fontWeight: FontWeight.w500
-                  //     ),
-                  //   ),
-                  //   icon: Icon(Icons.public, color: AppColors.gray, size: 28.dm),
-                  //   iconAlignment: AppCubit.get(context).locale.languageCode == 'en'? IconAlignment.start: IconAlignment.end,
-                  // ),
-                  Center(
-                    child: SvgPicture.asset(
-                      width: 550.0.w,
-                      height: 412.0.h,
-                      AppImages.houses,
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // TextButton.icon(
+                //   onPressed: () {
+                //     final nextLocale =
+                //         sl<CacheHelper>().getString(
+                //                   key: CacheKeys.languageCode,
+                //                 ) ==
+                //                 'en'
+                //             ? 'ar'
+                //             : 'en';
+                //     log("SignInForm: change language to $nextLocale");
+                //     AppCubit.get(context).changeLanguage(Locale(nextLocale));
+                //   },
+                //   label: Text(
+                //     AppCubit.get(context).locale.languageCode == 'en'
+                //         ? "العربيه"
+                //         : "English",
+                //     style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                //       fontSize: 22.sp,
+                //       fontWeight: FontWeight.w500
+                //     ),
+                //   ),
+                //   icon: Icon(Icons.public, color: AppColors.gray, size: 28.dm),
+                //   iconAlignment: AppCubit.get(context).locale.languageCode == 'en'? IconAlignment.start: IconAlignment.end,
+                // ),
+                SvgPicture.asset(
+                  width: 550.0.w,
+                  height: 412.0.h,
+                  AppImages.houses,
+                ),
+                EmailTextFormField(
+                  controller: cubit.emailController,
+                  label: 'Email'.tr(context),
+                ),
+                SizedBox(height: 10.0.h),
+                PasswordTextFormField(
+                  controller: cubit.passwordController,
+                  label: 'Password'.tr(context),
+                ),
+                // SizedBox(height: 20.0.h),
+                // Row(
+                //   children: [
+                //     Checkbox(
+                //       value: cubit.rememberMe,
+                //       activeColor: AppColors.primary100,
+                //       onChanged: (value) {
+                //         cubit.changeRememberMe(value);
+                //       },
+                //     ),
+                //     Text(
+                //       'Save Data for next time'.tr(context),
+                //       style: Theme.of(
+                //         context,
+                //       ).textTheme.bodyMedium!.copyWith(fontSize: 18.sp),
+                //     ),
+                //   ],
+                // ),
+                SizedBox(height: 40.0.h),
+                CustomFilledButton(
+                  width: double.infinity,
+                  height: 50.0.h,
+                  onPressed: () {
+                    if (cubit.formKey.currentState!.validate()) {
+                      cubit.signIn();
+                    }
+                  },
+                  title: 'Sign In'.tr(context),
+                  icon: SvgPicture.asset(
+                    AppIcons.arrowLeftCircle,
+                    colorFilter: ColorFilter.mode(
+                      AppColors.white,
+                      BlendMode.srcIn,
                     ),
                   ),
-                  SizedBox(
-                    child: EmailTextFormField(
-                      controller: cubit.emailController,
-                      label: 'Email'.tr(context),
-                    ),
-                  ),
-                  SizedBox(height: 10.0.h),
-                  PasswordTextFormField(
-                    controller: cubit.passwordController,
-                    label: 'Password'.tr(context),
-                  ),
-                  // SizedBox(height: 20.0.h),
-                  // Row(
-                  //   children: [
-                  //     Checkbox(
-                  //       value: cubit.rememberMe,
-                  //       activeColor: AppColors.primary100,
-                  //       onChanged: (value) {
-                  //         cubit.changeRememberMe(value);
-                  //       },
-                  //     ),
-                  //     Text(
-                  //       'Save Data for next time'.tr(context),
-                  //       style: Theme.of(
-                  //         context,
-                  //       ).textTheme.bodyMedium!.copyWith(fontSize: 18.sp),
-                  //     ),
-                  //   ],
-                  // ),
-                  SizedBox(height: 20.0.h),
-                  CustomFilledButton(
-                    width: double.infinity,
-                    height: 50.0.h,
-                    onPressed: () {
-                      if (cubit.formKey.currentState!.validate()) {
-                        cubit.signIn();
-                      }
-                    },
-                    title: 'Sign In'.tr(context),
-                    fontSize: 18,
-                  ),
-                ],
-              ),
+                  fontSize: 18,
+                ),
+              ],
             ),
           ),
         );
