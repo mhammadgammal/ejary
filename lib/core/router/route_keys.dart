f4588b6d3a28a0dc818210806ecb2d37eb463db7
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-27 20:49:39 +0200

    Add new route keys for settings, alarms, and add property screens

--- lib/core/router/route_keys.dart
+++ lib/core/router/route_keys.dart
@@ -1,6 +1,9 @@
 abstract class RouteKeys {
   static const String signIn = '/sign_in';
   static const String home = '/home';
+  static const String settings = '/settings';
+  static const String alarms = '/alarms';
+  static const String addProperty = '/add_property';
   static const String propertyDetails = '/property_details';
   static const String alarmDetails = '/alarm_details';
 }
