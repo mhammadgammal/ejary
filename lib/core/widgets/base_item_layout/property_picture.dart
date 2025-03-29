e10916d5d9d77597152fb3f181d3753ca3c04ead
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-29 14:15:14 +0200

    add empty placeholder for property item if no property picture

--- lib/core/widgets/base_item_layout/property_picture.dart
+++ lib/core/widgets/base_item_layout/property_picture.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:ejary/core/assets/app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PropertyPicture extends StatelessWidget {
   const PropertyPicture({super.key, required this.imagePath});
@@ -13,13 +15,29 @@ class PropertyPicture extends StatelessWidget {
     return Container(
       width: 232.w,
       height: 170.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-        image: DecorationImage(
-          image: FileImage(File(imagePath)),
-          fit: BoxFit.cover,
-        ),
-      ),
+      decoration:
+          imagePath.isNotEmpty
+              ? BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                image: DecorationImage(
+                  image: FileImage(File(imagePath)),
+                  fit: BoxFit.cover,
+                ),
+              )
+              : null,
+      child:
+          imagePath.isEmpty
+              ? Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 0),
+                  child: SvgPicture.asset(
+                    AppIcons.picture,
+                    width: 48.0.w,
+                    height: 48.0.h,
+                  ),
+                ),
+              )
+              : null,
     );
   }
 }
