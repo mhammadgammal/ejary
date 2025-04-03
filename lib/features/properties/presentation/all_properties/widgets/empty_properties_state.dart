f88fa42ac957510ffe74659073f4fd2ef0c55673
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-04-03 14:30:39 +0200

    feat: Add new property button and enhance empty properties state UI

--- lib/features/properties/presentation/all_properties/widgets/empty_properties_state.dart
+++ lib/features/properties/presentation/all_properties/widgets/empty_properties_state.dart
@@ -6,34 +6,50 @@ import 'package:ejary/core/widgets/buttons/filled_buttons/custom_filled_button.d
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EmptyPropertiesState extends StatelessWidget {
   const EmptyPropertiesState({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 10.0.h,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SvgPicture.asset(
-          width: 162.5.w,
-          height: 150.0.h,
-          AppIcons.homeModernIc,
-          colorFilter: ColorFilter.mode(AppColors.gray50, BlendMode.srcIn),
-        ),
-        Text('No properties currently..add it from here'.tr(context)),
-        CustomFilledButton(
-          width: 248.w,
-          height: 48.h,
-          title: 'add_property_here'.tr(context),
-          fillColor: AppColors.secondary,
-          onPressed: () {
-            AppCubit.get(context).changeTabIndex(1);
-          },
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(top: 120.0.h),
+      child: Column(
+        spacing: 30.0.h,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            width: 162.5.w,
+            height: 150.0.h,
+            AppIcons.homeModernIc,
+            colorFilter: ColorFilter.mode(AppColors.gray50, BlendMode.srcIn),
+          ),
+          Text(
+            'No properties currently..add it from here'.tr(context),
+            style: GoogleFonts.tajawal(
+              color: AppColors.gray50,
+              fontSize: 32.0.sp,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          CustomFilledButton(
+            width: 248.w,
+            height: 48.h,
+            title: 'add_new_property'.tr(context),
+            fillColor: AppColors.secondary,
+            icon: SvgPicture.asset(
+              AppIcons.plus,
+              colorFilter: ColorFilter.mode(AppColors.white, BlendMode.srcIn),
+            ),
+            iconAlignment: IconAlignment.start,
+            onPressed: () {
+              AppCubit.get(context).changeTabIndex(1);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
