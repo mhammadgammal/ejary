543f99276d221ee40d1a0b1ec99e064d4f4b1370
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-04-04 05:35:35 +0200

    feat: Improve Alarm Display and Add New String Resource
    -   Update the `alarm_message` string resource in the Arabic localization file (`ar.json`) to "رسالة تذكير!".
    -   Refactor `AlarmWidget` to display alarm information within a styled container, including borders and background color.
    -   Modify layout and style of `AlarmWidget` to improve the visibility and presentation.
    -   Move the date display to be inline with the alarm message in the `AlarmWidget`.
    -   Adjust the leading icon in the `AlarmWidget` to use a `Container` with a background color instead of an `IconButton`.

--- lib/features/alarms/widgets/alarm_widget.dart
+++ lib/features/alarms/widgets/alarm_widget.dart
@@ -14,39 +14,61 @@ class AlarmWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.close, color: Colors.white),
-        style: ButtonStyle(
-          backgroundColor: WidgetStatePropertyAll(AppColors.gray100),
-          shape: WidgetStatePropertyAll(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.background100,
+        borderRadius: BorderRadius.circular(8.0.r),
+        border: Border.all(color: AppColors.gray200, width: 1.0.w),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 16.0.h),
+      margin: EdgeInsets.symmetric(horizontal: 24.0.w, vertical: 10.0.h),
+      child: ListTile(
+        leading: Container(
+          width: 24.0.w,
+          height: 24.0.h,
+          decoration: BoxDecoration(
+            color: AppColors.gray100,
+            borderRadius: BorderRadius.circular(2.0.r),
           ),
+          child: Icon(Icons.close, color: Colors.white),
         ),
-      ),
-      title: Text(
-        AppStrings.alarmMessage.tr(context),
-        style: GoogleFonts.tajawal(
-          fontSize: 18.sp,
-          fontWeight: FontWeight.bold,
-          color: AppColors.primary100,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              AppStrings.alarmMessage.tr(context),
+              style: GoogleFonts.tajawal(
+                fontSize: 18.sp,
+                fontWeight: FontWeight.bold,
+                color: AppColors.primary100,
+              ),
+            ),
+            Text(
+              DateFormat('d/M/yyyy الساعة h:mm a', 'ar').format(date),
+              style: GoogleFonts.tajawal(
+                fontSize: 13.sp,
+                fontWeight: FontWeight.w500,
+                color: AppColors.gray200,
+              ),
+            ),
+          ],
         ),
-      ),
-      trailing: Text(
-        DateFormat('d/M/yyyy الساعة h:mm a', 'ar').format(date),
-        style: GoogleFonts.tajawal(
-          fontSize: 13.sp,
-          fontWeight: FontWeight.w500,
-          color: AppColors.gray200,
-        ),
-      ),
-      subtitle: Text(
-        '${AppStrings.renterName.tr(context)} $renterName ${AppStrings.timeMessage.tr(context)}',
-        style: GoogleFonts.tajawal(
-          fontSize: 16.sp,
-          fontWeight: FontWeight.w500,
-          color: AppColors.gray200,
+        // trailing:
+        // Text(
+        //   DateFormat('d/M/yyyy الساعة h:mm a', 'ar').format(date),
+        //   style: GoogleFonts.tajawal(
+        //     fontSize: 13.sp,
+        //     fontWeight: FontWeight.w500,
+        //     color: AppColors.gray200,
+        //   ),
+        // ),
+        subtitle: Text(
+          '${AppStrings.renterName.tr(context)} $renterName ${AppStrings.timeMessage.tr(context)}',
+          style: GoogleFonts.tajawal(
+            fontSize: 16.sp,
+            fontWeight: FontWeight.w500,
+            color: AppColors.gray200,
+          ),
         ),
       ),
     );
