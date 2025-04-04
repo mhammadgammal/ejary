0e3e99c18601991242709e808b88e8c9bb0137da
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-04-04 19:58:57 +0200

    feat: Enhance AttachFileButton with filled button option and Lottie animation

--- lib/features/apartments/presentation/add_edit_apartment/widgets/attach_file_button.dart
+++ lib/features/apartments/presentation/add_edit_apartment/widgets/attach_file_button.dart
@@ -1,12 +1,15 @@
 import 'package:ejary/core/assets/app_icons.dart';
+import 'package:ejary/core/assets/app_lottie.dart';
 import 'package:ejary/core/theme/app_color.dart';
 import 'package:ejary/core/utils/extensions/string_extensions.dart';
 import 'package:ejary/core/widgets/buttons/custom_outlined_button_with_border.dart';
+import 'package:ejary/core/widgets/buttons/filled_buttons/custom_filled_button.dart';
 import 'package:ejary/features/apartments/presentation/add_edit_apartment/widgets/cubit/attach_file_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 
 class AttachFileButton extends StatelessWidget {
   const AttachFileButton({
@@ -28,28 +31,38 @@ class AttachFileButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AttachFileCubit, AttachFileState>(
       builder: (context, state) {
-        return CustomOutlinedButtonWithBorder(
-          width: (width ?? 250.0).w,
-          height: (height ?? 50.0).h,
-          onPressed: () {
-            AttachFileCubit.get(context).pickProfilePic();
-          },
-          title:
-              AttachFileCubit.get(context).fileName.isEmpty
-                  ? title.tr(context)
-                  : AttachFileCubit.get(context).fileName,
-          fontSize: fontSize,
-          fontWeight: fontWeight,
-          titleColor: AppColors.primary100,
-          borderColor: AppColors.primary100,
-          icon: SvgPicture.asset(
-            AppIcons.downloadIc,
-            colorFilter: ColorFilter.mode(
-              AppColors.primary100,
-              BlendMode.srcIn,
-            ),
-          ),
-        );
+        return AttachFileCubit.get(context).fileName.isEmpty
+            ? CustomOutlinedButtonWithBorder(
+              width: (width ?? 250.0).w,
+              height: (height ?? 50.0).h,
+              onPressed: () {
+                AttachFileCubit.get(context).pickProfilePic();
+              },
+              title: title.tr(context),
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+              titleColor: AppColors.primary100,
+              borderColor: AppColors.primary100,
+              icon: SvgPicture.asset(
+                AppIcons.downloadIc,
+                colorFilter: ColorFilter.mode(
+                  AppColors.primary100,
+                  BlendMode.srcIn,
+                ),
+              ),
+            )
+            : CustomFilledButton(
+              width: (width ?? 250.0).w,
+              height: (height ?? 50.0).h,
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+              title: AttachFileCubit.get(context).fileName,
+              fillColor: AppColors.primary100,
+              icon: Lottie.asset(AppLottie.success, repeat: false),
+              onPressed: () {
+                AttachFileCubit.get(context).pickProfilePic();
+              },
+            );
       },
     );
   }
